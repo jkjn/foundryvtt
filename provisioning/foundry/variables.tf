10100810ef04c29eb4053070d1b72e5d4d16369f
jkjn <jshane.mueller@gmail.com>
2022-08-16 19:46:31 -0500

    variable

--- provisioning/foundry/variables.tf
+++ provisioning/foundry/variables.tf
@@ -2,3 +2,7 @@ variable "default_prefix" {
     type = string
     description = "Default prefix to apply to resources"
 }
+variable "environment" {
+    type = string
+    description = "The environment to deploy to"
+}
